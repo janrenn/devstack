cee284ecec1d876888ecdf1e47e062a5792614e1
janrenn <renner@cyberfox.cz>
2018-12-27 02:19:15 +0100

    rm

---
+++ test/index.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en-US" itemscope itemtype="http://schema.org/WebSite" prefix="og: http://ogp.me/ns#">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<script nonce="xxxxxxxxxxxxxxxxxxxx">
+		window.DEBUG = true;
+		window.LANG = 'en';
+		window.LOCALE = 'en_US';
+		window.JANRENN = { timer: { firstByte: (new Date()).getTime(), now: function () { return (new Date()).getTime() - JANRENN.timer.firstByte; } } };
+		window.console && window.DEBUG && console.log('Hello, call me the first byte.', (window.JANRENN ? JANRENN.timer.now() : ''));
+		window.dataLayer = window.dataLayer || [];
+	</script>
+
+		<title>test</title>
+
+	<!-- meta -->
+	<meta name="author" content="CEBRE">
+	<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=auto">
+	<!-- https://developers.google.com/web/updates/2013/12/300ms-tap-delay-gone-away -->
+	<!-- https://www.sitepoint.com/5-ways-prevent-300ms-click-delay-mobile-devices/ -->
+	<meta name="geo.position" content="50.847262;4.369813">
+	<meta name="apple-mobile-web-app-title" content="CEBRE">
+	<meta name="application-name" content="CEBRE">
+	<meta name="msapplication-TileImage" content="https://cebre.cz/mstile-144x144.png">
+	<meta name="msapplication-TileColor" content="#005c95">
+	<meta name="theme-color" content="#005c95">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="google-site-verification" content="LcSB_nWprQwSoJmV6eEIm4QD6wr3oL5eLDQcKAzmnsA">
+
+	<!-- link -->
+	<link rel="image_src" href="https://cebre.cz/static/2017/01/homepage-1200x800.jpg">
+	<link rel="shortcut icon" href="/favicon.ico">
+	<link rel="icon" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="icon" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#005c95">
+	<link rel="manifest" href="/manifest.json">
+	<link rel="search" type="application/opensearchdescription+xml" title="CEBRE" href="https://cebre.cz/opensearch.xml">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="/bundle/css/bootstrap.min.css?xxxxxxxxxxxxxxxxxxxx"><!--tohle je cíl..., možná /bundle/css/global.css  ...-->
+	<link rel="stylesheet" href="/bundle/css/global.min.css?xxxxxxxxxxxxxxxxxxxx">
+	<link rel="stylesheet" href="/bundle/css/components.min.css?xxxxxxxxxxxxxxxxxxxx">
+	<link rel="stylesheet" href="/bundle/css/homepage.min.css?xxxxxxxxxxxxxxxxxxxx">
+
+	<!-- fonts -->
+
+	<!-- Google Tag Manager -->
+	<script nonce="xxxxxxxxxxxxxxxxxxxx">(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-NNLBT9F');</script>
+	<!-- End Google Tag Manager -->
+</head>
+<body id="test" class="no-js">
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NNLBT9F"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	<script src="/bundle/js/critical.min.js?xxxxxxxxxxxxxxxxxxxx" nonce="xxxxxxxxxxxxxxxxxxxx">
+		window.console && window.DEBUG && console.log('Hello, critical JS has been parsed.', (window.JANRENN ? JANRENN.timer.now() : ''));
+	</script>
+
+	<div class="main-wrapper" id="top">
+		<h1><a href="/">test</a></h1>
+		<p>keepin' your browsers happy</p>
+	</div>
+
+	<!-- JavaScript -->
+	<script src="/bundle/js/jquery.min.js?xxxxxxxxxxxxxxxxxxxx"></script>
+	<script src="/bundle/js/bootstrap.min.js?xxxxxxxxxxxxxxxxxxxx"></script>
+	<script src="/bundle/js/global.min.js?xxxxxxxxxxxxxxxxxxxx"></script>
+	<script src="/bundle/js/homepage.min.js?xxxxxxxxxxxxxxxxxxxx"></script>
+</body>
+</html>
